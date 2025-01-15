3b8f420286be1ee1a893cfb0d6646f2f11587e57
Hongli Wen <work@whl.moe>
2025-01-15 11:34:04 +0800

    Update build.rs

--- build.rs
+++ build.rs
@@ -4,12 +4,14 @@ fn main() {
     let current_dir = env::current_dir().expect("Failed to get current directory");
 
     let cpp_lib_path = current_dir.join("cpp/build-x64-windows-vulkan-release/lib/static");
+    let cpp_lib_path_vs = current_dir.join("cpp/build-x64-windows-vulkan-release/lib/static/Release");
 
     println!("cargo:rerun-if-changed={}", cpp_lib_path.display());
 
     println!("cargo:rustc-cfg=feature=\"crt-static\"");
 
     println!("cargo:rustc-link-search=native={}", cpp_lib_path.display());
+    println!("cargo:rustc-link-search=native={}", cpp_lib_path_vs.display());
 
     let vulkan_lib_path = env::var("VULKAN_SDK").expect("VULKAN_SDK environment variable not set");
     let vulkan_lib_path = PathBuf::from(vulkan_lib_path).join("Lib");
